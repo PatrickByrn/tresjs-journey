da7de0dc3e27ebd071a93961c99ab526118058e2
Patrick Byrn <pbyrn@asu.edu>
2023-05-19 22:08:02 -0700

    fix(lesson-20): finished lesson 20 added more cont
    Finished lesson 20 and added more controls.

--- pages/index.vue
+++ pages/index.vue
@@ -34,7 +34,7 @@
         <TresPoints>
           <TresBufferGeometry :position="[positions, 3]"> </TresBufferGeometry>
           <TresPointsMaterial
-            :color="meshColor"
+            :color="particleColor"
             :sizeAttenuation="true"
             :size="0.03"
           ></TresPointsMaterial>
@@ -58,6 +58,7 @@
 import { useTweakPane } from "@tresjs/cientos";
 import { TresCanvas, useLoader, useRenderLoop } from "@tresjs/core";
 import { useParallax, useWindowSize } from "@vueuse/core";
+import gsap from "gsap";
 import {
   BasicShadowMap,
   Group,
@@ -87,11 +88,10 @@ const sectionMeshes = ref<Object3D[]>([]);
 const setSectionMeshes = (el: any) => {
   if (el && !sectionMeshes.value.includes(el)) {
     sectionMeshes.value.push(el);
-    console.log("added" + el);
   }
 };
 
-// Texture gradient
+// Texture gradient & mesh color
 const textureGradient = ref();
 textureGradient.value = await useLoader(TextureLoader, "3.jpg").then(function (
   response
@@ -99,33 +99,39 @@ textureGradient.value = await useLoader(TextureLoader, "3.jpg").then(function (
   response.magFilter = NearestFilter;
   return response;
 });
+const meshColor = ref("#ffeded");
+pane.addInput(meshColor, "value", { label: "Mesh Color" });
 
 // object placement logic
-const objectsDistance = ref(4);
-pane.addInput(objectsDistance, "value");
+const objectsDistanceY = ref(4);
+pane.addInput(objectsDistanceY, "value", { label: "Object Distance Y" });
+const objectsDistanceX = ref(2);
+pane.addInput(objectsDistanceX, "value", { label: "Object Distance X" });
 const generateMeshDistance = (index: number) => {
-  return -objectsDistance.value * index;
+  return -objectsDistanceY.value * index;
 };
 const placeObjects = (i: number) => {
   sectionMeshes.value[i].position.setY(generateMeshDistance(i));
   if (i % 2 == 0) {
-    sectionMeshes.value[i].position.setX(2);
+    sectionMeshes.value[i].position.setX(objectsDistanceX.value);
   } else {
-    sectionMeshes.value[i].position.setX(-2);
+    sectionMeshes.value[i].position.setX(-objectsDistanceX.value);
   }
 };
-watch(objectsDistance, async (newDistance, oldDistance) => {
-  for (let i = 0; i < sectionMeshes.value?.length!; i++) {
-    placeObjects(i);
+watch(
+  [objectsDistanceY, objectsDistanceX],
+  async (newDistance, oldDistance) => {
+    for (let i = 0; i < sectionMeshes.value?.length!; i++) {
+      placeObjects(i);
+    }
   }
-});
-
-const meshColor = ref("#ffeded");
-pane.addInput(meshColor, "value");
+);
 
 //particles
 const particlesCount = ref(200);
-pane.addInput(particlesCount, "value");
+pane.addInput(particlesCount, "value", { label: "Particle Count" });
+const particleColor = ref("#ffeded");
+pane.addInput(particleColor, "value", { label: "Particle Color" });
 
 const positions = ref<Float32Array>();
 const generateParticles = (count: number) => {
@@ -133,14 +139,30 @@ const generateParticles = (count: number) => {
   for (let i = 0; i < count; i++) {
     positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
     positions[i * 3 + 1] =
-      objectsDistance.value * 0.5 -
-      Math.random() * objectsDistance.value * sectionMeshes.value.length;
+      objectsDistanceY.value * 0.5 -
+      Math.random() * objectsDistanceY.value * sectionMeshes.value.length;
     positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
   }
   return positions;
 };
-watch(particlesCount, (newCount, oldCount) => {
+watch([particlesCount, objectsDistanceY], ([newCount, oldCount]) => {
   positions.value = generateParticles(newCount);
+});
+
+// scroll into view logic
+const currentSection = ref<number>();
+watch(scrollY, (newY, oldY) => {
+  currentSection.value = Math.round(newY! / height.value);
+});
+watch(currentSection, (newSection, oldSection) => {
+  console.log(newSection);
+  gsap.to(sectionMeshes.value[newSection!].rotation, {
+    duration: 1.5,
+    ease: "power2.inOut",
+    x: "+=6",
+    y: "+=3",
+    z: "+=1.5",
+  });
 });
 
 onLoop(({ delta, elapsed, clock }) => {
@@ -149,7 +171,6 @@ onLoop(({ delta, elapsed, clock }) => {
     for (let i = 0; i < sectionMeshes.value?.length!; i++) {
       sectionMeshes.value[i].rotation.x += delta * 0.5;
     }
-
     const parallaxX = tilt.value;
     const parallaxY = roll.value;
 
@@ -158,7 +179,7 @@ onLoop(({ delta, elapsed, clock }) => {
       typeof scrollY.value != "undefined"
     ) {
       camera.value.position.y =
-        -(scrollY.value / height.value) * objectsDistance.value;
+        -(scrollY.value / height.value) * objectsDistanceY.value;
     }
 
     if (typeof cameraGroup.value != "undefined") {
@@ -173,6 +194,9 @@ onMounted(() => {
     placeObjects(i);
   }
   positions.value = generateParticles(particlesCount.value);
+  currentSection.value = scrollY.value
+    ? Math.round(scrollY.value! / height.value)
+    : 0;
   window.addEventListener("scroll", () => {
     scrollY.value = window.scrollY;
   });
